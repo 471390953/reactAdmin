062fad5d5b89c6cc7f547f4166d2ceed9113aa0a
M <471390953@qq.com>
2020-06-29 10:30:18 +0800

    less

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+ 
+ /**
+  * 
+  *按需加载
+  */
+ const { override, fixBabelImports } = require('customize-cra');
+
+ module.exports = function override(config, env) {
+  // do stuff with the webpack config...
+   return config;
+};
+module.exports = override(  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',    
+    style: 'css',
+   }),
+ );
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+import './login.less'
+// import './login.css'
+
+// 登录组件
+export default class Login extends Component{
+    render(){
+        return(
+            <div className="login">
+                <header className="login-header">ss</header>
+                <section className="login-content"></section>
+            </div>
+        )
+    }
+}
+
